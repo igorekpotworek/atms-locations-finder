e6f73634faca62119605f885a4924223e09c0dd1
igorekpotworek <igipopko@gmail.com>
2020-09-18 21:31:20 +0200

    upgrade gradle

--- run.sh
+++
@@ -1,7 +0,0 @@
-# Please put a proper bash command that will build and run you application
-# For example if you build a Java application:
-# mvn clean package
-# java -jar target/your-application-1.0-SNAPSHOT.jar
-echo Building and running the application
-./gradlew clean build
-java -jar build/libs/atms-locations-finder-1.0-SNAPSHOT.jar
